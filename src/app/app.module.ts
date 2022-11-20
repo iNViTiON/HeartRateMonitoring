f562ea4d98d9fc7b84884faa72df827e6089c76f
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2022-11-20 15:13:15 +0700

    chore: enable Yarn PnP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
-import { PushModule } from "@rx-angular/template/push";
-import { LetModule } from "@rx-angular/template/let";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ViewportPrioModule } from '@rx-angular/template';
+import { LetModule } from '@rx-angular/template/let';
+import { PushModule } from '@rx-angular/template/push';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule,
-    LetModule,
-    PushModule,
-    ViewportPrioModule,
-  ],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, LetModule, PushModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
